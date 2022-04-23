3492cfc2bab8aff54c734673d07a4b0b20855caa
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-23 19:20:44 +0200

    Setup /api/tips

--- pages/api/tips.ts
+++ pages/api/tips.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type Tip = {
+  title: string
+  description: string
+  author: string
+}
+
 type Data = {
-  name: string
+  tips: Tip[]
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const tip = {
+    title: "Tip",
+    description: "This is a tip",
+    author: "John Doe",
+  };
+
+  res.status(200).json({ tips: [tip] });
 }
