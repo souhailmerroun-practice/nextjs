95bd8bd8d8d9d7c5a63efb4e341c834731e779ab
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-23 19:50:38 +0200

    Code refactor using a file to load data

--- pages/api/tips.ts
+++ pages/api/tips.ts
@@ -1,28 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Data } from '../types';
+import { Data, Tip } from '../types';
+import fs from "fs";
+import path from "path";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const tip = {
-    title: "Tip",
-    description: "This is a tip",
-    author: "John Doe",
-  };
 
-  const tip2 = {
-    title: "Another tip",
-    description: "This is a random one",
-    author: "Jane Doe",
-  };
+  const fileData = fs.readFileSync(path.resolve('data/tips.json'), "utf8");
+  const jsonData = JSON.parse(fileData);
 
-  const tip3 = {
-    title: "Here is it",
-    description: "Lorem ipsum something",
-    author: "Jane Jana",
-  };
-
-  res.status(200).json({ tips: [tip, tip2, tip3] });
+  res.status(200).json({ tips: jsonData });
 }
